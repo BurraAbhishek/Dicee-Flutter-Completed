5d8ee138dfb9bd8e06024a4275aaddaa80b1cf2a
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-03-23 13:55:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,9 +7,10 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue,
         appBar: AppBar(
           title: Text('Dicee'),
+          centerTitle: true,
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -23,41 +25,87 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 1;
-  int rightDiceNumber = 1;
-
-  void changeDiceFace() {
-    setState(() {
-      leftDiceNumber = Random().nextInt(6) + 1;
-      rightDiceNumber = Random().nextInt(6) + 1;
-    });
+  int leftDiceNumber = (Random.secure().nextInt(6))+1;
+  int rightDiceNumber = (Random.secure().nextInt(6))+1;
+  var rollText = 'You have not yet rolled the dice.';
+  void printDice() {
+    int totalDice = leftDiceNumber + rightDiceNumber;
+    if(leftDiceNumber==rightDiceNumber) {
+      rollText = 'You have rolled doubles of $leftDiceNumber . \n This means that you rolled $totalDice.';
+    } else {
+      rollText = 'You have rolled $leftDiceNumber and $rightDiceNumber . \n This means that you rolled $totalDice.';
+    }
   }
-
+  void rollDice() {
+    leftDiceNumber = (Random.secure().nextInt(6))+1;
+    rightDiceNumber = (Random.secure().nextInt(6))+1;
+    printDice();
+  }
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              child: Image.asset(
-                'images/dice$leftDiceNumber.png',
+          Padding(
+            padding: EdgeInsets.all(4.0),
+            child: Text(
+              'Press the button to roll the two dice!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 32.0,
+                color: Colors.yellowAccent,
+                fontWeight: FontWeight.bold,
               ),
-              onPressed: () {
-                changeDiceFace();
-              },
             ),
           ),
-          //Get students to create the second die as a challenge
-          Expanded(
-            child: FlatButton(
-              child: Image.asset(
-                'images/dice$rightDiceNumber.png',
-              ),
-              onPressed: () {
-                changeDiceFace();
-              },
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Image.asset('images/dice$leftDiceNumber.png'),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Image.asset('images/dice$rightDiceNumber.png'),
+                  ),
+                ),
+              ],
             ),
+          ),
+          Text(
+            '$rollText',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 24.0,
+              color: Colors.yellowAccent,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          FlatButton(
+              onPressed: () {
+                setState(() {
+                  rollDice();
+                });
+              },
+              child: Padding(
+                padding: EdgeInsets.all(4.0),
+                child: Text(
+                  ' Roll the dice. ',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 32.0,
+                    color: Colors.indigoAccent,
+                    fontWeight: FontWeight.bold,
+                    backgroundColor: Colors.yellowAccent,
+                  ),
+                ),
+              ),
           ),
         ],
       ),
